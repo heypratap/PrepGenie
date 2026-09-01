182f283f8dec2d9f637108a91efe5aab54fe7476
Pratap Yadav <pratapyadav522@gmail.com>
2026-09-01 16:26:25 +0530

    Fix production CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,34 +3,42 @@ const cookieParser = require("cookie-parser")
 const cors = require("cors")
 
 const authRouter = require("./routes/auth.routes")
-const interviewRouter =
-    require("./routes/interview.routes")
+const interviewRouter = require("./routes/interview.routes")
 
 const app = express()
 
-//
-//
 
-
-
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://prepgenie-82u2rdk0z-heypratap.vercel.app/login"
+        ],
+        credentials: true
+    })
+)
 
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
 
 app.get("/", (req, res) => {
     res.json({
-        message: "PrepGenie API is running"
+        message: "PrepGenie AI is running"
     })
 })
 
 
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        status: "OK",
+        message: "PrepGenie backend is running"
+    })
+})
+
 
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
+
 
 module.exports = app
